06fe3d2055c2618c02e6a008173e0e1d8f5391be
Tuomas Mäkinen <tuomas.makinen@equilibrium.co>
2023-09-29 12:27:21 +0300

    import -> reconstruct

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn cli() -> Command {
         .subcommand_required(true)
         .arg_required_else_help(false)
         .subcommand(
-            Command::new("import")
-                .about("Import state")
+            Command::new("reconstruct")
+                .about("Reconstruct L2 state")
                 .subcommand_required(true)
                 .subcommand(
                     Command::new("l1")
-                        .about("Import state from Ethereum L1")
+                        .about("Read state from Ethereum L1")
                         .arg(
                             arg!(--"start-block" <START_BLOCK>)
                                 .help("Ethereum block number to start state import from")
@@ -27,7 +27,7 @@ fn cli() -> Command {
                 )
                 .subcommand(
                     Command::new("file")
-                        .about("Import state from file")
+                        .about("Read state from file")
                         .arg(arg!(<FILE> "File to import state from"))
                         .arg_required_else_help(true),
                 ),
@@ -38,17 +38,17 @@ fn main() {
     let matches = cli().get_matches();
 
     match matches.subcommand() {
-        Some(("import", sub_matches)) => {
+        Some(("reconstruct", sub_matches)) => {
             match sub_matches.subcommand() {
                 Some(("l1", args)) => {
                     let start_block = args.get_one::<u64>("start-block").expect("required");
                     let block_step = args.get_one::<u64>("block-step").expect("required");
-                    println!("import from L1, starting from block number {}, processing {} blocks at a time", start_block, block_step);
+                    println!("reconstruct from L1, starting from block number {}, processing {} blocks at a time", start_block, block_step);
                     // TODO(tuommaki): Implement block fetch logic.
                 }
                 Some(("file", args)) => {
                     let input_file = args.get_one::<String>("FILE").expect("required");
-                    println!("import from file (path: \"{}\")", input_file);
+                    println!("reconstruct from file (path: \"{}\")", input_file);
                 }
                 _ => unreachable!(),
             }
