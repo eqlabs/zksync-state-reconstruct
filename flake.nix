8df2c0498e44e9833f5ef19a29d300e812c8dbc3
Jonathan <94441036+zeapoz@users.noreply.github.com>
2024-01-04 15:37:18 +0100

    chore: nix env (#53)

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rust-toolchain =
+        pkgs.rust-bin.selectLatestNightlyWith
+        (toolchain:
+          toolchain.default.override {
+            extensions = ["rust-src"];
+          });
+    in {
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            rust-toolchain
+            rust-analyzer
+            cargo-sort
+            openssl
+            pkg-config
+            clang
+            protobuf
+          ];
+
+          LIBCLANG_PATH = "${pkgs.llvmPackages_11.libclang.lib}/lib";
+        };
+    });
+}
