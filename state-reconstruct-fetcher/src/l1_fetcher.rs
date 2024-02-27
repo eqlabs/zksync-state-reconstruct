23da5fc3b672f62f2b6ac29c1baa91f553788ac9
Vaclav Barta <vbarta@mangrove.cz>
2024-02-27 14:08:24 +0100

    spelling BLOCK_STEP

--- state-reconstruct-fetcher/src/l1_fetcher.rs
+++ state-reconstruct-fetcher/src/l1_fetcher.rs
@@ -335,7 +335,7 @@ impl L1Fetcher {
 
                     let next_l1_block_number = current_l1_block_number + U64::from(BLOCK_STEP);
                     if next_l1_block_number > end_block_number {
-                        // Some of the BLOCKSTEP blocks asked for in this iteration
+                        // Some of the `BLOCK_STEP` blocks asked for in this iteration
                         // probably didn't exist yet, so we set `current_l1_block_number`
                         // appropriately as to not skip them.
                         if current_l1_block_number < end_block_number {
