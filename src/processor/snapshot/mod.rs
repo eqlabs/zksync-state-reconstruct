2e25b52a3f23e01b0a2fe9f88012524302dcc279
zeapoz <zeapo@pm.me>
2024-08-15 12:50:51 +0200

    chore: update snapshot header file name

--- src/processor/snapshot/mod.rs
+++ src/processor/snapshot/mod.rs
@@ -22,7 +22,7 @@ use tokio::sync::mpsc;
 use super::Processor;
 
 pub const DEFAULT_DB_PATH: &str = "snapshot_db";
-pub const SNAPSHOT_HEADER_FILE_NAME: &str = "snapshot-header.json";
+pub const SNAPSHOT_HEADER_FILE_NAME: &str = "snapshot_header.json";
 pub const SNAPSHOT_FACTORY_DEPS_FILE_NAME_SUFFIX: &str = "factory_deps.proto.gzip";
 
 pub struct SnapshotBuilder {
