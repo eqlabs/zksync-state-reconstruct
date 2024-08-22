b787de9da12a4fc5dd6fcf6abf061a0aa441d9ad
Jonathan <94441036+zeapoz@users.noreply.github.com>
2024-08-22 15:43:27 +0200

    chore(snapshots): use correct l1 batch number of initial write (#115)
    * chore(snapshots): use correct l1 batch number of initial write
    
    * chore: clippy

--- src/processor/snapshot/mod.rs
+++ src/processor/snapshot/mod.rs
@@ -9,7 +9,7 @@ use async_trait::async_trait;
 use ethers::types::{Address, H256, U256, U64};
 use eyre::Result;
 use state_reconstruct_fetcher::{
-    constants::{ethereum, storage, zksync::L2_BLOCK_NUMBER_ADDRESSES},
+    constants::{storage, zksync::L2_BLOCK_NUMBER_ADDRESSES},
     types::CommitBlock,
 };
 use state_reconstruct_storage::{
@@ -74,9 +74,7 @@ impl Processor for SnapshotBuilder {
                     .insert_storage_log(&mut SnapshotStorageLog {
                         key: *key,
                         value,
-                        l1_batch_number_of_initial_write: U64::from(
-                            block.l1_block_number.unwrap_or(0),
-                        ),
+                        l1_batch_number_of_initial_write: U64::from(block.l1_batch_number),
                         enumeration_index: 0,
                     })
                     .expect("failed to insert storage_log_entry");
@@ -226,7 +224,7 @@ fn reconstruct_genesis_state(database: &mut SnapshotDatabase, path: &str) -> Res
         database.insert_storage_log(&mut SnapshotStorageLog {
             key,
             value,
-            l1_batch_number_of_initial_write: U64::from(ethereum::GENESIS_BLOCK),
+            l1_batch_number_of_initial_write: U64::from(0),
             enumeration_index: 0,
         })?;
     }
